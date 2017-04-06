09bd4649ef94215656e518743054469d37c249fc
tusharkailash <tusharkailash29@gmail.com>
2017-04-06 16:04:18 -0700

    Update duplicates.py

--- duplicates.py
+++ duplicates.py
@@ -1,8 +1,9 @@
 # -Remove-Duplicates-from-Sorted-Array-
 
-For example,
+"""For example,
 Given input array nums = [1,1,2],
-Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. 
+It doesn't matter what you leave beyond the new length."""
 
 
 

2106475caf4d22547be1d46d7aefc420d899114c
tusharkailash <tusharkailash29@gmail.com>
2017-04-06 21:05:00 -0700

    Create allduplicates.py

---
+++ allduplicates.py
@@ -0,0 +1,25 @@
+#Find All Duplicates in an Array
+"""
+Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+Find all the elements that appear twice in this array. Could you do it without extra space and in O(n) runtime?
+Example: Input: [4,3,2,7,8,2,3,1]
+Output: [2,3]
+"""
+
+class Solution(object):
+    def findDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = []
+        for n in nums:
+            if nums[abs(n) - 1] < 0:
+                ans.append(abs(n))
+            else:
+                nums[abs(n) - 1] *= -1
+        return ans
+
+nums = [[4,3,2,7,8,2,3,1]
+ans = Solution().findDuplicates(nums)
+print ans

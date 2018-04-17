13a94c0346bb1386549fdd3fded882c1d1ca09a3
tusharkailash <tusharkailash29@gmail.com>
2018-04-16 18:03:18 -0700

    another approach added

--- Strings/reverse_sring2.py
+++ Strings/reverse_sring2.py
@@ -9,23 +9,39 @@
 
 			###Best Solution:
 
-class Solution(object):
-    def reverseStr(self, s, k):
-        """
-        :type s: str
-        :type k: int
-        :rtype: str
-        """
-        s = list(s)
-        for i in range(0, len(s), 2*k):    #left
-            j = min(i+k-1,len(s)-1)                              #no need to actually use min. just writing i+k-1 works too
-            while i < j:                   #right
-                s[i] , s[j] = s[j] , s[i]
-                i += 1
-                j -= 1
-        return "".join(s)        
+# class Solution(object):
+#     def reverseStr(self, s, k):
+#         """
+#         :type s: str
+#         :type k: int
+#         :rtype: str
+#         """
+#         s = list(s)
+#         for i in range(0, len(s), 2*k):    #left
+#             j = min(i+k-1,len(s)-1)                              #no need to actually use min. just writing i+k-1 works too
+#             while i < j:                   #right
+#                 s[i] , s[j] = s[j] , s[i]
+#                 i += 1
+#                 j -= 1
+#         return "".join(s)
                    
 ###second solution:
+
+# class Solution(object):
+#     def reverseStr(self, s, k):
+#         """
+#         :type s: str
+#         :type k: int
+#         :rtype: str
+#         """
+#         s = list(s)
+#         for i in range(0,len(s),2*k):
+#             s[i:i+k]=reversed(s[i:i+k])
+#         return "".join(s)
+#
+
+
+###Solution:
 
 class Solution(object):
     def reverseStr(self, s, k):
@@ -35,11 +51,12 @@ class Solution(object):
         :rtype: str
         """
         s = list(s)
-        for i in range(0,len(s),2*k):
-            s[i:i+k]=reversed(s[i:i+k])
-        return "".join(s)
-      
-        
+        for i in range(0, len(s), 2*k):
+            s[i:i+k] = reversed(s[i:i+k])
+            print s
+        # return "".join(s)
+
+
 s = "abcdefghij"
 k=2
 ans= Solution().reverseStr(s,k)
